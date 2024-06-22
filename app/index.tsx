6e7937a5a17505b2250b6d07d55681730dfe7244
EmekaAgara <emekaagara@gmail.com>
2024-06-22 11:19:35 +0100

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,60 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+StyleSheet;
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <View style={styles.buttons}>
+        <Link
+          href={"/screens/home"}
+          style={{ flex: 1, backgroundColor: "black" }}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+              Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+        <Link
+          href={"/signup"}
+          style={{ flex: 1, backgroundColor: "#fff" }}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: "500" }}>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    color: "white",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginBottom: 60,
+    paddingHorizontal: 20,
+  },
+});
+
+export default index;
